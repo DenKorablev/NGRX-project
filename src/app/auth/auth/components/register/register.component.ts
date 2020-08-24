a409390f1b0d03b6075d71cc0a4999eb7b4b803f
DenKorablev <denis18051990@mail.ru>
2020-08-24 22:34:58 +0300

    first change

---
+++ src/app/auth/auth/components/register/register.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from '@angular/core'
+import {FormGroup, FormBuilder, Validators} from '@angular/forms'
+import {Store} from '@ngrx/store'
+import {registerAction} from '../../store/actions/register.action'
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.scss'],
+})
+export class RegisterComponent implements OnInit {
+  form: FormGroup
+
+  constructor(private fb: FormBuilder, private store: Store) {}
+
+  ngOnInit(): void {
+    this.initializeForm()
+  }
+
+  initializeForm(): void {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      email: '',
+      password: '',
+    })
+  }
+
+  onSubmit(): void {
+    this.store.dispatch(registerAction(this.form.value))
+  }
+}
